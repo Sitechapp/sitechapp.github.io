09aa3ea86c83597208e5477114bb0602cc3244c0
Sitechapp <98682054+Sitechapp@users.noreply.github.com>
2022-11-24 23:26:27 -0800

    Add files via upload

--- Boutique/js/Boutique.js
+++ Boutique/js/Boutique.js
@@ -1,19 +1,21 @@
-function openMenu(){
-  var nav = document.getElementById("nav");
-  nav.style.width = "250px";
-  if(nav.style.width === "250px"){
-      nav.style.left = "0px";
-      nav.style.display = 'block';
+
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
   }
 }
-
-function main(){
-  var nav = document.getElementById("nav");
-  nav.style.width = "0px";
-  if(nav.style.width === "0px"){
-      nav.style.left = "-10px";
-  }
-  
-}
-
-
--- js/App.js
+++ js/App.js
@@ -1,5 +1,6 @@
 var hom = document.getElementById('home');
-var what = document.getElementById("whatsapp")
+var what = document.getElementById("whatsapp");
+var main = document.getElementById("main");
 
 
 
@@ -22,13 +23,12 @@ function closeMenu(){
     
 }
 
-function main(){
+  maind.onclick = () => {
   var nav = document.getElementById("nav");
   nav.style.width = "0px";
   if(nav.style.width === "0px"){
       nav.style.left = "-10px";
   }
-  
 }
 
 
@@ -49,6 +49,36 @@ function closeform(){
   html.style.display = 'block';
 }
 
+
+//----------------------boutton home-----------------//
+function home(){
+  var progress = document.getElementById('myProgress');
+  var i = 0;
+  progress.style.display = 'block';
+if (i == 0) {
+  i = 1;
+  var elem = document.getElementById("myBar");
+  var width = 0;
+  var id = setInterval(frame, 0);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      
+      i = 0;
+      setTimeout(() => {
+        //document.location.reload(true);
+        progress.style.display = 'none';
+        window.location ='/index.html';
+      }, 2000);
+    }else {
+      width++;
+      elem.style.width = width + "%";
+      elem.innerHTML = width + "%";
+      
+    }
+  }
+}
+}
 
 //Boutton rafraichir la page
 function reload(){
@@ -79,10 +109,37 @@ function reload(){
   }
   }
 
-
+  //Boutton rafraichir la page
+function boutique(){
+  var progress = document.getElementById('myProgress');
+  var i = 0;
+  progress.style.display = 'block';
+if (i == 0) {
+  i = 1;
+  var elem = document.getElementById("myBar");
+  var width = 0;
+  var id = setInterval(frame, 0);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      
+      i = 0;
+      setTimeout(() => {
+        window.location = '/Boutique/boutique.html';
+      }, 2000);
+    }else {
+      width++;
+      elem.style.width = width + "%";
+      elem.innerHTML = width + "%";
+      
+    }
+  }
+}
+}
+var what =document.getElementById('whatsapp');
   //boutton open whatsapp
   function whatspp(){
-    what.style.height = "auto";
+    document.getElementById('whatsapp').style.height = "auto";
     if(what.style.height === "auto"){
         what.style.bottom = "0px";
         //what.style.display = 'block';
@@ -90,7 +147,7 @@ function reload(){
     }
     //boutton open whatsapp
     function btnWhat(){
-    what.style.height = "0px";
+      document.getElementById('whatsapp').style.height = "0px";
         if(what.style.width === "0px"){
             what.style.left = "-10px";
         }
